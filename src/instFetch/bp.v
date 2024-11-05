c10187c8c4585d9709e69d15f7ffa0663c54f0d4
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-11-05 12:07:54 +0800

    code: bp

--- src/instFetch/bp.v
+++ src/instFetch/bp.v
@@ -1,4 +1,4 @@
-`define BP_SIZE_W 4
+`define BP_SIZE_W 7
 `define BP_SIZE (1 << `BP_SIZE_W)
 
 module BranchPredictor(
@@ -15,6 +15,15 @@ module BranchPredictor(
 
   // now pc
   input wire [31:0] pc_in,
+
+  // depends of regs
+  input wire [3:0] rs1_tag,
+  input wire [3:0] rs2_tag,
+
+  // broadcasted tag from cdb
+  input wire [3:0] cdb_tag,
+  // broadcasted value from cdb
+  input wire [31:0] cdb_val,
   
   // whether former prediction is wrong
   output wire predict_fail,
@@ -25,6 +34,44 @@ module BranchPredictor(
   output wire [31:0] branch_addr
 );
 
+reg [54:0] bp_table [0:`BP_SIZE - 1]; // {op, rs1, rs1_tag, rs2, rs2_tag, imm}
+reg [31 - 2 - `BP_SIZE_W:0] pc_tag_table [0:`BP_SIZE - 1];
+reg busy [0:`BP_SIZE - 1];
+reg [1:0] predict_fst [0:`BP_SIZE - 1]; // 00: SNT, 01: WNT, 10: WT, 11: ST
+
+wire need_predict = branch && rdy_in;
+wire [`BP_SIZE_W - 1:0] idx = pc_in[1 + `BP_SIZE_W:2];
+wire has_former_fst = busy[idx] && (pc_tag_table[idx] == pc_in[31:2 + `BP_SIZE_W]);
+
+assign need_branch = need_predict ?
+          (op == `JAL ? 1'b1 :
+          (has_former_fst ? (predict_fst[idx] > 2'b01) : 1'b0)) : 1'b0;
+
+assign branch_addr = need_branch ? pc_in + imm : 32'b0;
+
+
+
+always @(posedge clk_in) begin
+  if (rst_in) begin
+    for (i = 0; i < `BP_SIZE; i = i + 1) begin
+      bp_table[i] <= 55'b0;
+      pc_table[i] <= 32'b0;
+      busy[i] <= 1'b0;
+      predict_fst[i] <= 2'b01; // reset to WNT state
+    end
+  end
+  if (!rdy_in) begin
+    // pause
+  end
+  else begin
+    if (need_predict && !has_former_fst && op != `JAL) begin // store predict infos
+      busy[idx] <= 1'b1;
+      predict_fst[idx] <= 2'b01;
+      pc_tag_table[idx] <= pc_in[31:2 + `BP_SIZE_W];
+      bp_table[idx] <= {op, rs1, rs1_tag, rs2, rs2_tag, imm};
+    end
+  end
+end
 
 
 endmodule
